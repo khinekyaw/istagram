9dc8540e0dc273557c92f6195112cf8cf38807e1
DanielKhine <danielrio126@gmail.com>
2021-11-25 11:24:57 +0630

    update backend

--- backend/istagram/manage.py
+++ backend/istagram/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'istagram_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'istagram.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
